d6f324dc29db05a5a603fc0b38e244b721c4c63e
Romina Cepele <rcepele@unitsystem.net>
2022-06-13 16:27:16 +0200

    day1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello!")
+//using require method
+//const numbers = require("./numbers.json")
+
+//console.log(numbers);
+let val;
+let arr2 = [6,7,8];
+//Using the fs module
+const fs = require("fs");
+//read users.json file
+fs.readFile("numbers.json", function(err,data) {
+    //check for errors
+    if (err) throw err;
+
+    //converting to Json
+    const numbers = JSON.parse(data);
+
+    let array3 = numbers.concat(arr2);
+    let sum = 0;
+    for (let i = 0 ; i<array3.length ; i++){
+        sum = sum + array3[i];
+    }
+
+    console.log(numbers);
+    console.log(array3);
+    console.log(sum);
+});
